92ea8bc464d552518b2291b57296792bd93c9823
Ryan McAlpin <rhmcalpin@gmail.com>
2017-09-15 20:37:37 -0700

    refactor to use proper unsubscribe technique

--- src/app/my-stories/my-stories.component.ts
+++ src/app/my-stories/my-stories.component.ts
@@ -1,28 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import * as firebase from 'firebase/app';
+import { FirebaseListObservable } from 'angularfire2/database';
+
+import 'rxjs/add/operator/takeUntil';
+import { Subject } from 'rxjs/Subject';
 
 import { AuthService } from '../auth.service';
 import { StoryService } from '../story.service';
-import { FirebaseListObservable } from 'angularfire2/database';
+import { Story } from '../story.model';
 
 @Component({
   selector: 'app-my-stories',
   templateUrl: './my-stories.component.html',
   styleUrls: ['./my-stories.component.css']
 })
-export class MyStoriesComponent implements OnInit {
+export class MyStoriesComponent implements OnInit, OnDestroy {
   user: any;
   stories: FirebaseListObservable<any[]>;
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
+
 
   constructor(private authService: AuthService,
               private storyService: StoryService) { }
 
   ngOnInit() {
     firebase.auth().onAuthStateChanged(user => {
-      this.user = user ? user : null;
-      this.stories = this.storyService.getStories(this.user.uid);
+      if (user) {
+        this.user = user;
+        this.getStories(this.user.uid);
+      } else {
+        this.user = null;
+      }
     });
   }
 
+  getStories(uid: string) {
+    this.storyService.getStories(uid)
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(stories => this.stories = stories);
+  }
+
+  selectStory(story) {
+    
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 }
--- src/app/story.service.ts
+++ src/app/story.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Story } from './story.model';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-
-
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class StoryService {
-  stories: FirebaseListObservable<any[]>;
-  //uid: string;
 
   constructor(private database: AngularFireDatabase) {
   }
 
   getStories(uid) {
     return this.database.list('games/' + uid);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('And error occurred', error); //demo only
+    return Promise.reject(error.message || error);
   }
 }
